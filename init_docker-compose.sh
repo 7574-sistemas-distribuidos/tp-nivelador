c7e94b5eca966764248707bf080794efd8c07100
Micaela <mkaimakamian@fi.uba.ar>
2026-08-31 15:08:55 -0300

    Ejercicio N°1: Se crea script para definir contenedores  de clientes dinamicamente. (OPCIONAL)

---
+++ init_docker-compose.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# This script is used to initialize the docker-compose environment for the project.
+
+CLIENTES="$1"
+SERVER_PORT=5678
+FILE_NAME="docker-compose.yaml"
+
+if [ -z "$CLIENTES" ] || [ "$CLIENTES" -le 0 ]; then
+  echo "No client specified. Please provide the amount of clients as arguments."
+  exit 1
+fi
+
+cat << EOF > $FILE_NAME
+services:
+  server:
+    build:
+      context: ./services/server
+      dockerfile: Dockerfile
+    container_name: server
+    environment:
+      - PYTHONUNBUFFERED=1
+      - SERVER_HOST=server
+      - SERVER_PORT=$SERVER_PORT
+
+EOF
+
+for ((i=0; i<CLIENTES; i++)); do
+  cat << EOF >> $FILE_NAME
+  client_$i:
+    build:
+      context: ./services/client
+      dockerfile: Dockerfile
+    container_name: client_$i
+    depends_on:
+      - server
+    environment:
+      - AGENCY_ID=$i
+      - SERVER_HOST=server
+      - SERVER_PORT=$SERVER_PORT
+EOF
+done
