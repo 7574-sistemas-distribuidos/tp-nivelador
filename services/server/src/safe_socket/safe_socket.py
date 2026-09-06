2d8822bdc386f333027d85fdb1ab306d919ec45f
Nazarena Rueda <50343570+Naza26@users.noreply.github.com>
2026-09-06 12:54:53 -0300

    implement short read tolerant socket within server service

--- services/server/src/safe_socket/safe_socket.py
+++ services/server/src/safe_socket/safe_socket.py
@@ -3,8 +3,30 @@ import socket
 # TODO: Complete with a short-read/short-write tolerant implementation
 
 
-def recv_all(socket: socket.socket, size):
-    return socket.recv(size)
+def recv_all(socket: socket.socket, totalBytesToReceive):
+    bytes_counter = 0
+    received_byte_data = socket.recv(totalBytesToReceive)
+
+    if received_byte_data == b"":
+        raise ConnectionError("Socket closed before receiving all data")
+
+    bytes_counter += len(received_byte_data)
+    
+    if bytes_counter == totalBytesToReceive:
+        return received_byte_data
+       
+    while bytes_counter < totalBytesToReceive:
+        remaining_bytes_to_receive = totalBytesToReceive - bytes_counter
+        new_received_data = socket.recv(remaining_bytes_to_receive)
+
+        if new_received_data == b"":
+            raise ConnectionError("Socket closed before receiving all data")
+
+        received_byte_data += new_received_data
+        bytes_counter += len(new_received_data)
+
+
+    return received_byte_data
 
 
 def send_all(socket: socket.socket, bytes):
