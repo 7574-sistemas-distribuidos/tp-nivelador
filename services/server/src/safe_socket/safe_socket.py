37cedfd077bba9b02fd89285fb5a4f428948a087
Nazarena Rueda <50343570+Naza26@users.noreply.github.com>
2026-09-06 14:24:47 -0300

    implement short write tolerant socket within server service

--- services/server/src/safe_socket/safe_socket.py
+++ services/server/src/safe_socket/safe_socket.py
@@ -1,22 +1,20 @@
 import socket
 
-# TODO: Complete with a short-read/short-write tolerant implementation
 
-
-def recv_all(socket: socket.socket, totalBytesToReceive):
+def recv_all(socket: socket.socket, total_bytes_to_receive):
     bytes_counter = 0
-    received_byte_data = socket.recv(totalBytesToReceive)
+    received_byte_data = socket.recv(total_bytes_to_receive)
 
     if received_byte_data == b"":
         raise ConnectionError("Socket closed before receiving all data")
 
     bytes_counter += len(received_byte_data)
-    
-    if bytes_counter == totalBytesToReceive:
+
+    if bytes_counter == total_bytes_to_receive:
         return received_byte_data
-       
-    while bytes_counter < totalBytesToReceive:
-        remaining_bytes_to_receive = totalBytesToReceive - bytes_counter
+
+    while bytes_counter < total_bytes_to_receive:
+        remaining_bytes_to_receive = total_bytes_to_receive - bytes_counter
         new_received_data = socket.recv(remaining_bytes_to_receive)
 
         if new_received_data == b"":
@@ -25,9 +23,22 @@ def recv_all(socket: socket.socket, totalBytesToReceive):
         received_byte_data += new_received_data
         bytes_counter += len(new_received_data)
 
-
     return received_byte_data
 
 
-def send_all(socket: socket.socket, bytes):
-    return socket.send(bytes)
+def send_all(socket: socket.socket, byte_data):
+    bytes_counter = 0
+    total_bytes_to_send = len(byte_data)
+    bytes_sent = socket.send(byte_data)
+
+    bytes_counter += bytes_sent
+
+    if bytes_counter == total_bytes_to_send:
+        return bytes_sent
+
+    while bytes_counter < total_bytes_to_send:
+        remaining_byte_data_to_send = byte_data[bytes_counter:]
+        bytes_sent = socket.send(remaining_byte_data_to_send)
+        bytes_counter += bytes_sent
+
+    return bytes_counter
